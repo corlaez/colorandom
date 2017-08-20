81b962c9beb88cf54adaeb188f804faf7badf5c4
lrn2prgrm <armanpela@gmail.com>
2017-08-20 13:11:51 -0500

    Network learns

--- src/main/java/com/company/nd/NDLayer.java
+++ src/main/java/com/company/nd/NDLayer.java
@@ -1,36 +1,41 @@
 package com.company.nd;
 
+import java.util.List;
+import java.util.Map;
 import java.util.function.UnaryOperator;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.factory.Nd4j;
-
-import static org.nd4j.linalg.ops.transforms.Transforms.*;
 
 /**
  * Created by jarma on 8/9/2017.
  */
 public class NDLayer {
 
-  public static boolean debug;
+  public static String DEFAULT_ACTIVATION = "leakyrelu";//https://github.com/deeplearning4j/nd4j/blob/aafbd0101b1a10a0d4dae1a2671895410a1708b2/nd4j-backends/nd4j-tests/src/test/java/org/nd4j/linalg/ops/DerivativeTests.java
 
-  int index = -1;
-  int nWeightsPerNeuron;
-  int nNeurons;
-  INDArray weights;
-  INDArray biases;
-  UnaryOperator<INDArray> activationFunction;
+  //metadata
+  int index = -1;//if set the index will be printed on toStrings
+  int nWeightsPerNeuron;//computed for clarity
+  int nNeurons;//computed for clarity
+  //data
+  INDArray weights;//bidimensional data array
+  INDArray biases;//data vector
+  UnaryOperator<INDArray> activationFunc;
+  UnaryOperator<INDArray> activationFuncDerivative;
+  //backprop
+  INDArray netInput;
+  INDArray activation;
+  INDArray weightsNabla;
+  INDArray biasesNabla;
 
   public NDLayer(int nWeightsPerNeuron, int nNeurons) {
     this.nNeurons = nNeurons;
     this.nWeightsPerNeuron = nWeightsPerNeuron;
-    if (debug) {
-      weights = Nd4j.randn(new int[]{nNeurons, nWeightsPerNeuron},12356L);
-      biases = Nd4j.randn(new int[]{nNeurons, 1}, 551615L);
-    } else {
-      weights = Nd4j.randn(new int[]{nNeurons, nWeightsPerNeuron});
-      biases = Nd4j.randn(new int[]{nNeurons, 1});
-    }
-    setActivationName(null);
+    this.weights = Nd4j.randn(new int[]{nNeurons, nWeightsPerNeuron});
+    this.biases = Nd4j.randn(new int[]{nNeurons, 1});
+    setActivationName(DEFAULT_ACTIVATION);
+    weightsNabla = Nd4j.zerosLike(weights);
+    biasesNabla = Nd4j.zerosLike(biases);
   }
 
   public NDLayer(int index, int numberOfWeightsPerNeuron, int numberOfNeurons) {
@@ -38,59 +43,42 @@ public class NDLayer {
     this.index = index;
   }
 
+  public INDArray net(INDArray input) {
+    if (nWeightsPerNeuron != input.size(0)) {
+      throw new IllegalArgumentException("wrong input");
+    }
+    netInput = weights.mmul(input).add(biases);
+    return netInput;
+  }
+
   public INDArray activate(INDArray input) {
-    INDArray output = weights.mmul(input).add(biases);
-    output = activationFunction.apply(output);
-    return output;
+    activation = activationFunc.apply(net(input));
+    return activation;
   }
 
-  public int getnNeurons() {
-    int r = weights.size(0);
-    if (r != nNeurons) {
-      throw new RuntimeException("NEURONS NUMBER IS WRONG: " + r + " != " + nNeurons);
-    }
-    return r;
+  public void setActivationName(String activation) {
+    this.activationFunc = x -> Nd4j.getExecutioner().execAndReturn(
+        Nd4j.getOpFactory().createTransform(activation, x));
+    this.activationFuncDerivative = x -> Nd4j.getExecutioner().execAndReturn(
+        Nd4j.getOpFactory().createTransform(activation, x).derivative());
+  }
+  //backprop
+  INDArray delta(INDArray result, INDArray target) {
+    return (result.sub(target)).mul(activationFuncDerivative.apply(result));
   }
 
-  public int getnWeigthsPerNeuron() {
-    int r = weights.getRow(0).length();
-    if (r != nWeightsPerNeuron) {
-      throw new RuntimeException("WPN NUMBER IS WRONG: " + r + " != " + nWeightsPerNeuron);
-    }
-    return r;
-  }
-
-  public void setActivationName(String activationNameP) {
-    if (activationNameP == null) {
-      activationFunction = x -> sigmoid(x);
-      return;
-    }
-    activationFunction = x -> Nd4j.getExecutioner().exec(
-        Nd4j.getOpFactory().createTransform(activationNameP, x)).z();
+  void updateLayer(float eta, int groupSize) {
+    weights.subi(weightsNabla.mul(eta / groupSize));
+    biases.subi(biasesNabla.mul(eta / groupSize));
+    weightsNabla = Nd4j.zerosLike(weights);
+    biasesNabla = Nd4j.zerosLike(biases);
   }
 
   @Override
   public String toString() {
     String s = "Layer" + (index > -1 ? " " + index : "") + ":\n";
     for (int i = 0; i < nNeurons; i++) {
-      s += "N" + i + ": w " + weights.getRow(i) +
-          ", b " + biases.getRow(i) + "\n";
-    }
-    return s;
-  }
-
-  public String toStringShort() {
-    String s = "Layer" + (index > -1 ? " " + index : "") + ":\n";
-    for (int i = 0; i < nNeurons; i++) {
-      s += "N" + i + "(" + nWeightsPerNeuron + ", " + biases.getRow(i) + ")\n";
-    }
-    return s;
-  }
-
-  public String toStringShortest() {
-    String s = "Layer" + (index > -1 ? " " + index : "") + ":\n";
-    for (int i = 0; i < nNeurons; i++) {
-      s += "N" + i + "(" + nWeightsPerNeuron + ")\n";
+      s += "N" + i + ": w " + weights.getRow(i) + ", b " + biases.getRow(i) + "\n";
     }
     return s;
   }
@@ -102,10 +90,25 @@ public class NDLayer {
   }
 
   public static void main(String[] args) {
-    NDLayer.debug = true;
-    NDLayer layer = new NDLayer(5, 3);
+    NDLayer layer = new NDLayer(5, 5);
+    INDArray input = Nd4j.rand(new int[]{5, 1});
+    System.out.println("Antes de entrenar");
     System.out.println(layer);
-    INDArray input = Nd4j.randn(new int[]{5, 1}, 1L);
-    System.out.println(layer.activate(input));
+    INDArray dif1 = input.sub(layer.activate(input));
+    System.out.println("dif " + dif1);
+    for (int i = 0; i < 1500; i++) {
+      layer.gradientDescent(input, input, 0.5f);
+    }
+    System.out.println("\nLuego de entrenar");
+    System.out.println(layer);
+    INDArray dif2 = input.sub(layer.activate(input));
+    System.out.println("dif " + dif2);
+  }
+
+  public void gradientDescent(INDArray in, INDArray target, float learningRate) {
+    INDArray bG = delta(in, target);
+    INDArray wG = bG.mmul(in.transpose());
+    weights.subi(wG.mul(learningRate));
+    biases.subi(bG.mul(learningRate));
   }
 }
--- src/main/java/com/company/nd/NDNetwork.java
+++ src/main/java/com/company/nd/NDNetwork.java
@@ -1,10 +1,13 @@
 package com.company.nd;
 
-import static org.nd4j.linalg.ops.transforms.Transforms.*;
+import static org.nd4j.linalg.ops.transforms.Transforms.pow;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.factory.Nd4j;
@@ -13,6 +16,7 @@ import org.nd4j.linalg.factory.Nd4j;
  * Created by jarma on 8/8/2017.
  */
 public class NDNetwork {
+
   List<NDLayer> layers;
 
   public NDNetwork(int... inputSizeAndlayersSizes) {
@@ -20,22 +24,22 @@ public class NDNetwork {
   }
 
   public NDNetwork(String optionalActivationFun, int... inputSizeAndlayersSizes) {
-    layers = new ArrayList<>();
-    for (int i = 1; i < inputSizeAndlayersSizes.length; i++) {
-      int numberOfWeightsPerNeuron = inputSizeAndlayersSizes[i - 1];
-      int numberOfNeurons = inputSizeAndlayersSizes[i];
-      layers.add(new NDLayer(i - 1, numberOfWeightsPerNeuron, numberOfNeurons));
-      layers.get(i - 1).setActivationName(optionalActivationFun);
+    if (inputSizeAndlayersSizes.length < 2) {
+      throw new IllegalArgumentException("enter at least input size and a layer size");
     }
-  }
-
-  public INDArray feedforward(Float... inputs) {
-    float[] ff = new float[inputs.length];
-    for (int i = 0; i < ff.length; i++) {
-      ff[i] = inputs[i];
+    NDLayer[] layers = new NDLayer[inputSizeAndlayersSizes.length - 1];
+    for (int i = inputSizeAndlayersSizes.length - 2; i >= 0; i--) {
+      int numberOfWeightsPerNeuron = inputSizeAndlayersSizes[i];
+      int numberOfNeurons = inputSizeAndlayersSizes[i + 1];
+      NDLayer layer = new NDLayer(i, numberOfWeightsPerNeuron, numberOfNeurons);
+      if (optionalActivationFun != null) {
+        layer.setActivationName(optionalActivationFun);
+      }
+      layers[i] = layer;
     }
-    INDArray x = Nd4j.create(ff);
-    return feedforward(x);
+    //output layer activation
+    layers[layers.length - 1].setActivationName("identity");
+    this.layers = Arrays.asList(layers);
   }
 
   public INDArray feedforward(INDArray x) {
@@ -45,24 +49,64 @@ public class NDNetwork {
     return x;
   }
 
-  public float error(INDArray in, INDArray expectedOut) {
-    INDArray out = feedforward(in);
-    return abs(expectedOut.sub(out)).getFloat(0);
+  public void UVSGD(Map<INDArray, INDArray> trainMap, int n, int groupSize, float eta, float etaT) {
+    List keys = new ArrayList(trainMap.keySet());
+    float numberOfGroups = (float) Math.ceil(trainMap.size() * 1.0d / groupSize);
+    float etaDelta = (eta - etaT) / (n - 1);
+    for (int i = 0; i < n; i++) {//n = epochs, the number of times we train from the map
+      Collections.shuffle(keys);//STOCHASTIC
+//      System.out.println("Epoch: " + i + "/" + n);
+      for (int j = 0; numberOfGroups > j; j++) {
+        boolean overflowed = groupSize * (j + 1) > keys.size();
+        int sizeOfGroup = overflowed ? keys.size() % groupSize : groupSize;
+        int init = j * groupSize;
+        int end = init + sizeOfGroup;
+        List<INDArray> keyStochasticGroup = keys.subList(init, end);//group = miniBatch
+        calculateNablas(trainMap, keyStochasticGroup);
+        for (NDLayer l : layers) {
+          l.updateLayer(eta, keyStochasticGroup.size());
+        }
+      }
+      eta -= etaDelta;
+    }
   }
 
-  @Override
-  public String toString() {
-    List<List<String>> ls = layers
-        .stream()
-        .map(l -> l.toStringShortest().split("\n"))
-        .map(Arrays::asList)
-        .collect(Collectors.toList());
-    return "Network:\n" + ls.toString()
-        .replaceAll("\\[L", "\n\\[L")
-        .replaceAll("]]", "]\n]");
+  private void calculateNablas(Map<INDArray, INDArray> trainMap, List<INDArray> keys) {
+    //get gradient from this group of keys
+    for (INDArray x : keys) {
+      //feedfoward
+      INDArray y = feedforward(
+          x);//is this redundant in my architecture? Delta could do this but should it?
+      //backward pass
+      //Get delta from last layer. This is the only place delta makes sense to use.
+      NDLayer lastLayer = layers.get(layers.size() - 1);
+      INDArray delta = lastLayer.delta(y, trainMap.get(x));
+      INDArray secondlastActivation;
+      if (layers.size() - 2 >= 0) {
+        secondlastActivation = layers.get(layers.size() - 2).activation;
+      } else {
+        secondlastActivation = x;
+      }
+      lastLayer.biasesNabla.addi(delta);
+      lastLayer.weightsNabla.addi(delta.mmul(secondlastActivation.transpose()));
+      for (int i = layers.size() - 2; i >= 0; i--) {
+        INDArray prevActivation;
+        if (i > 0) {
+          prevActivation = layers.get(i - 1).activation;
+        } else {
+          prevActivation = x;
+        }
+        NDLayer currL = layers.get(i);
+        NDLayer nextL = layers.get(i + 1);
+        INDArray sp = currL.activationFuncDerivative.apply(currL.netInput);
+        delta = nextL.weights.transpose().mmul(delta).mul(sp);
+        currL.weightsNabla.addi(delta.mmul(prevActivation.transpose()));
+        currL.biasesNabla.addi(delta);
+      }
+    }
   }
 
-  public String toStringShort() {
+  public String toStringMini() {
     List<List<String>> ls = layers
         .stream()
         .map(l -> l.toStringMini().split("\n"))
@@ -73,7 +117,8 @@ public class NDNetwork {
         .replaceAll("]]", "]\n]");
   }
 
-  public String toStringLong() {
+  @Override
+  public String toString() {
     List<List<String>> ls = layers
         .stream()
         .map(l -> l.toString().split("\n"))
@@ -85,11 +130,38 @@ public class NDNetwork {
   }
 
   public static void main(String[] args) {//x -> w + b = y
-    NDNetwork net = new NDNetwork(2, 5, 3, 2);
-    INDArray x = Nd4j.create(new float[]{1f, 1f}, new int[]{2,1});
-    System.out.println(net.toStringLong());
-    System.out.println(x);
-    System.out.println(net.feedforward(x));
-    System.out.println(net.error(x, x));
+    NDLayer.DEFAULT_ACTIVATION = "leakyrelu";
+    NDNetwork net = new NDNetwork(784, 784);
+    Map<INDArray, INDArray> trainMap = new HashMap<>();
+    if (true)//GLOBAL DEBUG
+    {
+      Nd4j.getRandom().setSeed(123l);
+    }
+    for (int i = 0; i < 100; i++) {
+      INDArray x = Nd4j.rand(784, 1);
+      trainMap.put(x, x);
+    }
+    INDArray x = trainMap.keySet().stream().findAny().get();
+    System.out.println(net.toString());
+    System.out.println("inp " + x);
+    System.out.println("out " + net.feedforward(x));
+    System.out.println("err " + net.error(trainMap));
+
+    net.UVSGD(trainMap, 3, 50, 0.5f, 0.1f);
+
+    System.out.println("AFTER");
+    System.out.println(net.toString());
+    System.out.println("inp " + x);
+    System.out.println("out " + net.feedforward(x));
+    System.out.println("err " + net.error(trainMap));
+  }
+
+  public float error1(INDArray in, INDArray expectedOut) {
+    INDArray out = feedforward(in);
+    return pow(expectedOut.sub(out), 2).sumNumber().floatValue() / 2;
+  }
+
+  public double error(Map<INDArray, INDArray> validationMap) {
+    return validationMap.keySet().stream().mapToDouble(x -> error1(x, validationMap.get(x))).sum();
   }
 }
